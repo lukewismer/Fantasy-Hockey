033135229aa83dba09d1e9b42747d049de982a26
lukewismer <lukewismer77@gmail.com>
2023-03-30 13:39:49 -0600

    Test Actions

--- web/fantasy-hockey-app/src/App.js
+++ web/fantasy-hockey-app/src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to test again.
+          Edit <code>src/App.js</code> and save to test again 1.
         </p>
         <a
           className="App-link"
